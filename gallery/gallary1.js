9efb8b878a4a77262ceff6aff1f817227b5d41fc
wildpoptart <porcshe33@gmail.com>
2024-06-07 20:30:17 -0400

    new structure adding gallery

---
+++ gallery/gallary1.js
@@ -0,0 +1,23 @@
+class CarGallery extends HTMLElement{
+	constructor(){
+		super();
+
+		this.classList.add('galleryClass');
+
+		this.innerHTML = `
+			<div class="grid">
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+				<a href="https://www.stanborsh.xyz/img/lowres_yellow_integra.JPG" 		target="_blank"><img src="../img/lowres_yellow_integra.jpg" alt="yellow Integra"></a>
+			</div>
+		`;
+	}
+}
+
+customElements.define('car-gallery', CarGallery);
