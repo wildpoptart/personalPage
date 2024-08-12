d8cc664620be8ac00874b554de3a85ce5af9c57c
wildpoptart <porcshe33@gmail.com>
2024-08-12 01:26:36 -0400

    added tags

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ aboutLink.addEventListener('click', () => {
     aboutContent.classList.toggle('transform-active');
     aboutContent.classList.toggle('opacity-active');
 });
+
+const tagLinks = document.querySelectorAll('.tag-nav ul div');
+const logEntries = document.querySelectorAll('.log-entry');
+
+tagLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const tag = link.getAttribute('data-tag');
+    filterLogs(tag);
+  });
+});
+
+function filterLogs(tag) {
+  logEntries.forEach(entry => {
+    const entryTag = entry.querySelector('.tag').textContent.trim();
+    if (entryTag.includes(tag)) {
+        entry.classList.toggle('hidden');
+    }
+  });
+}
